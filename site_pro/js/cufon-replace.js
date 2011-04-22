bb6830f4bc494caa058d02630a8572ab707517f2
Mohammed Niyas <niyas.n@innoppl.com>
2011-04-22 03:36:41 -0400

    first commit

---
+++ site_pro/js/cufon-replace.js
@@ -0,0 +1,3 @@
+Cufon.replace('.sf-menu li a, h1, h2, h3, h4, h6, .free', { fontFamily: 'Lucida Bright', hover:true});
+Cufon.replace('h5, figcaption, blockquote', { fontFamily: 'Lucida BrightItalic'});
+Cufon.replace(' .definition a, .news strong a, .service strong a, .title, .clients strong, .staff strong, .title1, .dropcap1', { fontFamily: 'Lucida BrightD'});
